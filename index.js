c5e805bf98a7b5d03ec3503306b8554f039688e5
Olivier <olileger@users.noreply.github.com>
2024-08-29 09:56:51 +0000

    Express server with EJS template engine

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) =>
+{
+  res.render("index");
+});
+
+const port = process.env["PEB_LISTENING_PORT"] || 3000;
+app.listen(port, () =>
+{
+    console.log(`Server listening on http://localhost:${port}`);
+});
