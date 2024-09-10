35f632350f79c2eecf3c2f3ef2d2af996ae19805
Olivier <olileger@users.noreply.github.com>
2024-09-10 08:27:20 +0000

    Cleaning old files

---
+++ src/PebApp.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <h1>Hello, App !</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import PebApp from './PebApp.vue'
 
-createApp(App).mount('#app')
+createApp(PebApp).mount('#app')
